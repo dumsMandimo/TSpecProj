57108d96fba74f8889d36b394df09cb748ef5aae
cyber-tumi <tumindaba879@gmail.com>
2026-04-13 09:20:29 +0200

    Added admin dashboard and basic layout

--- my-app/src/router/AppRouter.jsx
+++ my-app/src/router/AppRouter.jsx
@@ -2,6 +2,9 @@ import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import SignupPage from '../pages/signupPage';
 import LoginPage from '../pages/loginPage';
 import ApplicantDashboard from '../components/applicantDashboard/Dashboard';
+import AdminLayout from '../pages/adminLayout';
+import AdminDashboard from '../pages/adminDashboard';
+
 
 export default function AppRouter() {
   return (
@@ -10,6 +13,11 @@ export default function AppRouter() {
         <Route path="/signup"                element={<SignupPage />} />
         <Route path="/login"                 element={<LoginPage />} />
         <Route path="/dashboard/applicant"   element={<ApplicantDashboard />} />
+
+        <Route path="/admin" element={<AdminLayout />}>
+          <Route index element={<AdminDashboard />} />
+        </Route>
+
         <Route path="/"                      element={<Navigate to="/signup" replace />} />
         <Route path="*"                      element={<Navigate to="/signup" replace />} />
       </Routes>
