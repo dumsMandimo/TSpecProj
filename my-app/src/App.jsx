386d2a22d6741f3d203d8391eba72057b267aed5
shibari-daddy <yanilaboni@gmail.com>
2026-04-13 06:56:32 +0200

    revert

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -1,22 +1,15 @@
 import MyApplications from "./components/applicantDashboard/MyApplications";
 import SignupPage from "./pages/signupPage";
-import LoginPage from "./pages/loginPage";
-import {BrowserRouter, Route, Routes} from "react-router-dom";
 
 function App() {
   return (
-   <BrowserRouter>
-
-    <Routes>
-
-      <Route path="/" element={<SignupPage />} />
-      <Route path="/loginPage" element={<LoginPage />} />
-      <Route path="/MyApplications" element={<MyApplications />} />
-
-    </Routes>
-      
-    </BrowserRouter>
+   <>
+    <SignupPage />
+    <LoginPage />
+    <MyApplications />
+       </>
   );
+ 
 }
 
 export default App;
