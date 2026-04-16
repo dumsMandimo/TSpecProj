f55d1947b8a59fe7fa2168a8ee92d69b8a72f51c
shibari-daddy <yanilaboni@gmail.com>
2026-04-16 22:08:32 +0200

    SAQA related API fixes

--- my-app/src/services/api.jsx
+++ my-app/src/services/api.jsx
@@ -2,42 +2,59 @@ const express = require("express");
 const cors = require("cors");
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3000; //weusenvironmentvariablefordeployment
 
 app.use(cors()); //cross origin resource sharing
 
-app.options("*", (req, res, next) =>{
-    res.header('Access-Control-Allow-Origin', "*")
-    res.header('Access-Control-Allow-Origin', "GET, PUT, POST,OPTIONS")
-    res.header('Access-Control-Allow-Origin', "Authorization, Content-Length, X-Requested-With")
-    res.send(200);
+app.options("*", (req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Origin", "GET, PUT, POST,OPTIONS");
+  res.header(
+    "Access-Control-Allow-Origin",
+    "Authorization, Content-Length, X-Requested-With",
+  );
+  res.send(200);
 });
 
 app.use(express.json());
 
-app.use(express.urlencoded({ extended: false}))
+app.use(express.urlencoded({ extended: false }));
 
 app.use((req, res, next) => {
-    console.log(`${req.method} ${req.path} - ${req.ip}`);
-    next();
-})
-
-app.get('/', (req, res) => {
-    res.sendFile(__dirname + "/index.js");
-
-})
-
-app.get('/form', (req, res) => {
-    res.sendFile(__dirname + "/views/form.html");
+  console.log(`${req.method} ${req.path} - ${req.ip}`);
+  next();
 });
 
-app.get('/:word/echo', (req, res) => {
-    res.json({ "echo": req.params.word })
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/index.jsx");
 });
 
-app.all('*', (req, res) => {
-    res.send("Invalid route");
+app.get("/form", (req, res) => {
+  res.sendFile(__dirname + "/views/form.html");
+});
+
+app.get("/:word/echo", (req, res) => {
+  res.json({ echo: req.params.word });
+});
+
+app.all("*", (req, res) => {
+  res.send("Invalid route");
 });
 
 app.listen(PORT, () => console.log(`Listening on ${PORT}`));
 
+//api to fetch vacancies from saqa
+
+async function searchApplications(vacancy) {
+  try {
+    const response = await fetch(`https://saqa.org.za/vacancies/`);
+
+    if (!response.ok) {
+      throw new Error("No opportunities found");
+    }
+
+    const data = await response.json();
+  } catch (err) {
+    errorMessage.textContent;
+  }
+}
