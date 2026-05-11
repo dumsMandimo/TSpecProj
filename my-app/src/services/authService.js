3bf0dbca28386ae3d3b1e0bfc2edb6f83e8461e5
dumsMandimo <duma.mandimo@gmail.com>
2026-05-11 14:44:32 +0200

    fix: correcting imports

--- my-app/src/services/authService.js
+++ my-app/src/services/authService.js
@@ -1,15 +1,18 @@
 import {
   signInWithPopup,
   GoogleAuthProvider,
+  createUserWithEmailAndPassword,
 } from "firebase/auth";
 
 import { auth, db } from "./firebase";
+
 import {
   doc,
   setDoc,
   getDoc,
 } from "firebase/firestore";
 
+// GOOGLE SIGNUP
 export const signUpWithGoogle = async (role, extraData = {}) => {
   const provider = new GoogleAuthProvider();
 
@@ -17,7 +20,6 @@ export const signUpWithGoogle = async (role, extraData = {}) => {
     prompt: "select_account",
   });
 
-  // Google login
   const result = await signInWithPopup(auth, provider);
   const user = result.user;
 
@@ -28,24 +30,52 @@ export const signUpWithGoogle = async (role, extraData = {}) => {
   const userRef = doc(db, "users", user.uid);
   const userSnap = await getDoc(userRef);
 
-  // Create user only if they don't exist
   if (!userSnap.exists()) {
     await setDoc(userRef, {
       uid: user.uid,
       email: user.email,
       role,
       ...extraData,
-      status: 'active',
+      status: "active",
       createdAt: new Date().toISOString(),
     });
+
     return { user, role };
   }
 
   const data = userSnap.data();
-//block removed users
-  if (data.status === 'removed'){
+
+  if (data.status === "removed") {
     await auth.signOut();
-    throw new Error('account-removed');
+    throw new Error("account-removed");
   }
+
   return { user, role: data.role };
+};
+
+// EMAIL/PASSWORD SIGNUP
+export const signUpWithEmail = async (
+  email,
+  password,
+  role,
+  extraData = {}
+) => {
+  const result = await createUserWithEmailAndPassword(
+    auth,
+    email,
+    password
+  );
+
+  const user = result.user;
+
+  await setDoc(doc(db, "users", user.uid), {
+    uid: user.uid,
+    email: user.email,
+    role,
+    ...extraData,
+    status: "active",
+    createdAt: new Date().toISOString(),
+  });
+
+  return { user, role };
 };
