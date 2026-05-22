cb52afddfab22c6fb1fd7786c2870a2eb65228a1
Duma Mandimo <83492110+dumsMandimo@users.noreply.github.com>
2026-05-22 15:39:27 +0200

    Add Firebase mocks to setupTests.js
    Added mocks for Firebase modules to setupTests.js to avoid ESM transform issues during testing.

--- my-app/src/setupTests.js
+++ my-app/src/setupTests.js
@@ -1,6 +1,44 @@
 const { TextEncoder, TextDecoder } = require("util");
-
 global.TextEncoder = TextEncoder;
 global.TextDecoder = TextDecoder;
-
 require("@testing-library/jest-dom");
+
+// Mock Firebase modules to avoid ESM transform issues
+jest.mock('firebase/app', () => ({
+  initializeApp: jest.fn(() => ({})),
+}));
+
+jest.mock('firebase/auth', () => ({
+  getAuth: jest.fn(() => ({})),
+  signInWithEmailAndPassword: jest.fn(),
+  signOut: jest.fn(),
+  onAuthStateChanged: jest.fn(),
+  createUserWithEmailAndPassword: jest.fn(),
+  sendPasswordResetEmail: jest.fn(),
+  GoogleAuthProvider: jest.fn(),
+  signInWithPopup: jest.fn(),
+}));
+
+jest.mock('firebase/firestore', () => ({
+  getFirestore: jest.fn(() => ({})),
+  collection: jest.fn(),
+  doc: jest.fn(),
+  getDoc: jest.fn(),
+  getDocs: jest.fn(),
+  setDoc: jest.fn(),
+  addDoc: jest.fn(),
+  updateDoc: jest.fn(),
+  deleteDoc: jest.fn(),
+  query: jest.fn(),
+  where: jest.fn(),
+  orderBy: jest.fn(),
+  limit: jest.fn(),
+  onSnapshot: jest.fn(),
+}));
+
+jest.mock('firebase/storage', () => ({
+  getStorage: jest.fn(() => ({})),
+  ref: jest.fn(),
+  uploadBytes: jest.fn(),
+  getDownloadURL: jest.fn(),
+}));
